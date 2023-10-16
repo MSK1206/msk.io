5a0b0a12d65c587a9d0e900e1b61f649df816986
MSK1206 <phantom7216@yahoo.co.jp>
2023-10-17 00:14:31 +0900

    Del: 不要なコメントを削除

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,6 @@ import prisma from '@/lib/prisma';
 
 export default async function Home() {
   const users = prisma?.user.findMany();
-  // Postテーブルから全てデータを取得
   const posts = prisma?.post.findMany();
   return (
     <main className='flex min-h-screen flex-col items-center justify-center p-24'>
@@ -14,22 +13,19 @@ export default async function Home() {
           <p className='hidden'>id: {user.id}</p>
           <p>著者: {user.name}</p>
           <p className='hidden'>email: {user.email}</p>
-          {/* Add more user attributes as needed */}
         </div>
       ))}
       <br />
-
-      {/* Postテーブルの結果の一覧を画面に出力する */}
       {(await posts).map((post, index) => (
-        <div key={post.id} className='bg-white m-2 w-[500px]'>
-          <p className='hidden'>id: {post.id}</p>
+        <div key={post.id_post} className='bg-white m-2 w-[500px]'>
+          <p className='hidden'>id: {post.id_post}</p>
 
           <p className='my-5 p-8 font-bold text-center text-2xl underline'>
             {post.title}
           </p>
           <p className='mb-5 p-8 text-lg'>{post.content}</p>
-          <p className='hidden'>published: {post.published + ``}</p>
-          <p className='hidden'>authorId: {post.authorId}</p>
+          <p className=''>published: {post.published + ``}</p>
+          <p className=''>authorId: {post.userId}</p>
         </div>
       ))}
     </main>
