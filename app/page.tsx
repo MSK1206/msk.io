dd4ddd1e138d59bdc52f4dd7d5375d19edec36df
MSK1206 <phantom7216@yahoo.co.jp>
2023-10-17 23:21:08 +0900

    Add: SignIn

--- app/page.tsx
+++ app/page.tsx
@@ -1,33 +1,27 @@
 import Image from 'next/image';
 import prisma from '@/lib/prisma';
+import SignIn from './components/functions/SignIn';
 
 export default async function Home() {
-  const users = prisma?.user.findMany();
-  const posts = prisma?.post.findMany();
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center p-24">
-      <h1 className="font-bold text-2xl">Post</h1>
-      <h1 className="font-bold text-2xl"></h1>
-      {(await users).map((user, index) => (
-        <div key={user.id} className="bg-white m-2 w-[500px]">
-          <p className="hidden">id: {user.id}</p>
-          <p>著者: {user.name}</p>
-          <p className="hidden">email: {user.email}</p>
+    <main className="hero bg-base-200 flex min-h-screen items-center justify-center">
+      <div className="hero-content flex-col items-center">
+        <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl">
+          <form className="card-body">
+            <div className="flex flex-col items-center justify-center">
+              <Image
+                src={'/mylogo.svg'}
+                className="animate-rotate-center mb-5"
+                alt="My Logo"
+                width={100}
+                height={100}
+                priority
+              />
+              <SignIn />
+            </div>
+          </form>
         </div>
-      ))}
-      <br />
-      {(await posts).map((post, index) => (
-        <div key={post.id_post} className="bg-white m-2 w-[500px]">
-          <p className="hidden">id: {post.id_post}</p>
-
-          <p className="my-5 p-8 font-bold text-center text-2xl underline">
-            {post.title}
-          </p>
-          <p className="mb-5 p-8 text-lg">{post.content}</p>
-          <p className="">published: {post.published + ``}</p>
-          <p className="">authorId: {post.userId}</p>
-        </div>
-      ))}
+      </div>
     </main>
   );
 }
