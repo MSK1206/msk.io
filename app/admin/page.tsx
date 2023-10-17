8f719d79e30b4bb14d45dc8447c79c3fe709c31e
MSK1206 <phantom7216@yahoo.co.jp>
2023-10-18 02:02:59 +0900

    Add:

---
+++ app/admin/page.tsx
@@ -0,0 +1,19 @@
+'use client';
+import { signOut } from 'next-auth/react';
+
+export default function page() {
+  return (
+    <div className="min-h-screen flex flex-col items-center justify-center">
+      <h1>Admin Page</h1>
+      <button
+        type="button"
+        onClick={() => signOut()}
+        className="btn btn-base-100 border border-slate-400 rounded-lg p-1 my-5 w-[250px]"
+      >
+        <span className="bg-clip-text hover:text-transparent bg-gradient-to-r from-pink-500 to-violet-500">
+          Sign out
+        </span>
+      </button>
+    </div>
+  );
+}
