65a1f1627eee56abf1d8dd84a8eb0cf1636db4e4
MSK1206 <phantom7216@yahoo.co.jp>
2023-10-22 01:08:36 +0900

    Del: callbacksを削除

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -19,13 +19,6 @@ const authOptions: NextAuthOptions = {
   ],
   secret: process.env.NEXTAUTH_SECRET,
   adapter: PrismaAdapter(prisma),
-  callbacks: {
-    async redirect({ url, baseUrl }) {
-      if (url.startsWith('/')) return `${baseUrl}${url}`;
-      else if (new URL(url).origin === baseUrl) return url;
-      return baseUrl;
-    },
-  },
   pages: {
     signIn: '/',
     signOut: '/',
